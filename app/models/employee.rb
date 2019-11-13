25d15083f5ad199f946992b47c68a00d7c38c5fe
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2019-11-13 14:56:17 +0530

    [#004] converting database to pdf is completed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,19 +1,20 @@
 class Employee < ApplicationRecord
   def self.to_pdf
-    Prawn::Document.new do
-      Employee.all.each do |employee|
-        text "name: #{employee.name}"
-        text "email: #{employee.email}"
-        text "phone_number: #{employee.phone_number}"
-        text "\n"
-        # text "name\t\temail\t\t\tphone_number"
-        # text "\n"
-        # text "#{employee.name}\t\t#{employee.email}\t\t\t#{employee.phone_number}"
-        # text "\n"
+    pdf = Prawn::Document.new do |pdf|
+      self.all.each do |employee|
+        pdf.text "name: #{employee.name}"
+        pdf.text "email: #{employee.email}"
+        pdf.text "phone_number: #{employee.phone_number}"
+        pdf.text "\n"
+        # pdf.text "name\t\temail\t\t\tphone_number"
+        # pdf.text "\n"
+        # pdf.text "#{employee.name}\t\t#{employee.email}\t\t\t#{employee.phone_number}"
+        # pdf.text "\n"
         # filename: "Employees"
         # type: "application/pdf"
         # desposition: "inline"
       end
-    end.render
+    end
+      pdf.render
   end
 end
